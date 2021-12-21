cc44214a711918a38c3dfdd89fc4e97f65356633
Christopher Lord <christopher@pliosoft.com>
2021-12-21 16:24:32 -0700

    wrap top-level commands

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod run;
 
 use clap::Parser;
 use color_eyre::eyre::Result;
+use eyre::WrapErr;
 
 #[derive(Parser, Debug)]
 #[clap(name = "regrate", about, version)]
@@ -31,12 +32,16 @@ enum Regrate {
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+
     match Regrate::parse() {
-        Regrate::Init(args) => init::init_repo(args),
-        Regrate::Commit(args) => commit::commit_current(args),
-        Regrate::Run(args) => run::run_migrations(args),
-        Regrate::Resolve => resolve::resolve_conflicts(),
-        Regrate::Generate(args) => gen::generate_completion(args),
+        Regrate::Init(args) => init::init_repo(args).wrap_err("initializing new repo"),
+        Regrate::Commit(args) => commit::commit_current(args).wrap_err("committing changes"),
+        Regrate::Run(args) => run::run_migrations(args).wrap_err("running migration"),
+        Regrate::Resolve => resolve::resolve_conflicts().wrap_err("resolving conflicts"),
+        Regrate::Generate(args) => {
+            gen::generate_completion(args).wrap_err("generating shell autocomplete files")
+        }
     }?;
+
     Ok(())
 }
