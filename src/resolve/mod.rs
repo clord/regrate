be33c2897d09e36917128ccaca69dcdac66a81ec
Christopher Lord <christopher@pliosoft.com>
2022-01-03 15:13:41 -0700

    update comment

--- src/resolve/mod.rs
+++ src/resolve/mod.rs
@@ -1,6 +1,11 @@
 use eyre::Result;
 
 pub fn resolve_conflicts() -> Result<()> {
+    // upstream is kept. we have to back up to the common ancestor and rebase them? merge them to a
+    // single commit that will be left in the user's 'current'? regrate/.gitattributes could be
+    // used to set a custom merge rule.
+
+    // OLD PLAN THAT WON'T DEAL WITH MULTIPLE MIGRATIONS TO MERGE:
     //  - in a temp directory,
     //  - create a new migration populated with my old 'current' migration
     //  - revert merge conflict on new current
